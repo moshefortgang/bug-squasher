28590d41fe2e426ea5b6f41e221e70a372467edb
moshefortgang <moshe0726@gmail.com>
2023-07-24 18:27:46 +0300

    Prevent bug go in scoring container

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,7 +2,8 @@ import BugPosition from "@/interfaces/BugPosition";
 
 export function randomPosition(): BugPosition {
 	const bugSize: number = 50;
-    const x: number = Math.floor(Math.random() * (window.innerWidth - bugSize));
-    const y: number = Math.floor(Math.random() * (window.innerHeight - bugSize));
+	const scoreContainerSize: number = 60;
+    const x: number = Math.floor(Math.random() * (window.innerWidth - bugSize)) ;
+    const y: number = Math.floor(Math.random() * (window.innerHeight - bugSize - scoreContainerSize )) + scoreContainerSize;
     return { x, y };
 };
